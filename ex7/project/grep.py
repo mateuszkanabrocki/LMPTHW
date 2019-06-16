c10a14734f6567f42dc529bbc7fa748587c8a3ca
Mateusz <mateusz02975@gmail.com>
2019-06-16 17:22:16 +0200

    Add ex7

---
+++ ex7/project/grep.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python3
+
+
+# help
+# grep hi file
+# grep "hi" file
+# grep hi *.txt
+# grep hi file*
+# grep a file*.txt
+# return also a file name:
+# "-" stands for standard input
+# -i
+
+# use re module to use regexp
+
+
+# grep -i "hello" file
+# grep -i "this.*one" file1.txt
+# grep -i ".*there.*line" file1.
+
+# grep -i search file
+# grep -i "ha*" file1.
+
+
+import argparse
+from sys import exit
+from pathlib import Path
+import re
+
+
+def parse():
+    help = "This module works like a grep cmdlet but it's much simpler and less functional."
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument('-i', action='store_true', help='ignore words case while searching')
+    parser.add_argument('search', help='a literal or a regex to search for')
+    parser.add_argument('file', type=str, help='a file name to search in')
+    args = parser.parse_args()
+    print(args, '\n')
+    return args
+
+
+def find_files(args):
+    files_found = sorted(Path('.').glob(args.file))
+    if not files_found:
+        print("No file found.")
+        exit(1)
+    return files_found
+
+    # # for the absolute file path:
+    # files_found_abs = []
+    # for file_found in files_found:
+    #     files_found_abs.append(file_found.resolve())
+    # return files_found_abs
+
+
+def main():
+    args = parse()
+    if args.file == "-":
+        standin = input("Type a text here:\n>")
+        standin_lines = standin.split('\n')
+        search(args, standin_lines)
+    else:
+        files_found_abs = find_files(args)
+        for found in files_found_abs:
+            try:
+                with open(str(found), 'r') as file:
+                    print('Found a file:', found)
+                    lines = file.readlines()
+                    search(args, lines)
+            except FileNotFoundError:
+                print("No such text file.")
+                exit(1)
+
+
+def search(args, lines):
+    search = args.search.strip('"')
+    print("Lines found:")
+    for line in lines:
+        if re.findall(args.search, line):
+            print(line)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+# string = 'hello 12 hi 89. Howdy 34' #  line
+# pattern = '\d+' #  args.search
+
+# result = re.findall(pattern, string) 
+# print(result)
+
+# # Output: ['12', '89', '34']
